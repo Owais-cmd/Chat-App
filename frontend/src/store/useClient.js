2c24c959a375854d921b3324fbdf18a09bffd50d
Owais Saud Tanveer <owaistab18@gmail.com>
2025-09-22 20:56:30 +0530

    one check

--- frontend/src/store/useClient.js
+++ frontend/src/store/useClient.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from "zustand/middleware";
 import { io } from "socket.io-client"
 
-const SERVER_URL = import.meta.env.MODE==="development" ?"http://localhost:3000":"https://chat-app-5d7t.onrender.com/"
+const SERVER_URL = import.meta.env.MODE==="development" ?"http://localhost:3000":"https://chat-app-5d7t.onrender.com"
 
 
 export const useClient = create(persist(
